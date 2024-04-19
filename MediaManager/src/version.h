cdce6d5bf3aceb6c31d2131888ef0fc08e8a1e7b
PaunescuAndrei <paunescuandreialexandru@gmail.com>
2024-04-19 22:40:06 +0300

    version bump

--- MediaManager/src/version.h
+++ MediaManager/src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define VERSION_COMMA       1,0,2
-#define VERSION_TEXT       "1.0.2"
+#define VERSION_COMMA       1,0,3
+#define VERSION_TEXT       "1.0.3"
 
 #define FILEVER_VSINFO      VERSION_COMMA
 #define FILEVER_TEXT        VERSION_TEXT
